e9f234395e2492d32e5fdcf22618a816ce723d2f
longatroniks <karlolongin13@gmail.com>
2025-05-04 17:29:11 +0200

    Remove unused tailwind tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,5 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#4545cf',       
-        secondary: '#ff5d64',     
-        tertiary: '#ffed57',
-        dark: '#3d3d3d',         
-        'light-blue': '#accede', 
-      },
-    },
-  },
   plugins: [],
 }
